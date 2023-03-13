c488191f9f7777511cbbaa561684de68e0cf9a5c
David E. Barrera <dbarrerap@live.com>
2023-03-13 13:37:06 -0500

    Binding de los controles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { ContactoFormComponent } from './components/contacto-form/contacto-form.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contacto-form/contacto-form.component.ts
+++ src/app/components/contacto-form/contacto-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITarea } from 'src/app/interfaces/ITarea';
 
 @Component({
   selector: 'app-contacto-form',
   templateUrl: './contacto-form.component.html',
   styleUrls: ['./contacto-form.component.css']
 })
-export class ContactoFormComponent {
+export class ContactoFormComponent implements OnInit {
+  tareaForm!: FormGroup
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.tareaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      fecha: [new Date(), Validators.required],
+      descripcion: ['', Validators.maxLength(255)],
+    })
+  }
+  ngOnInit(): void {
+  }
+
+  setTarea() {
+    // console.log(this.tareaForm)
+    const tarea: ITarea = {...this.tareaForm.value, realizado: false}
+    console.log(tarea)
+  }
 
 }
