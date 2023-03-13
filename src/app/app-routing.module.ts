ccd98a8af2ca86012107e076deba93afa35e3122
David E. Barrera <dbarrerap@live.com>
2023-03-13 11:32:26 -0500

    Agregar rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoFormComponent } from './components/contacto-form/contacto-form.component';
+import { ContactoListComponent } from './components/contacto-list/contacto-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactoListComponent
+  },
+  {
+    path: 'new',
+    component: ContactoFormComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/contacto-list/contacto-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-list',
+  templateUrl: './contacto-list.component.html',
+  styleUrls: ['./contacto-list.component.css']
+})
+export class ContactoListComponent {
+
+}
