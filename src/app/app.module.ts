bf31d974b99f3d2e3f4b7ee053e8c0b267db83be
David E. Barrera <dbarrerap@live.com>
2023-03-13 11:20:42 -0500

    Agregar Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactoListComponent } from './components/contacto-list/contacto-list.component';
+import { ContactoFormComponent } from './components/contacto-form/contacto-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactoListComponent,
+    ContactoFormComponent
   ],
   imports: [
     BrowserModule,
